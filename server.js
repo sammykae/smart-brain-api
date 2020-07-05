2e1550cbbdd4d3ee243b66c83d0a4dd4187c212c
sammykae <sammykay301@live.com>
2020-07-05 22:35:02 +0100

    update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const knex=require('knex')
 const db =knex({
     client: 'pg',
     connection: {
-        connectionString: process.env.DATABASE_URL,
+        connectionString : process.env.DATABASE_URL,
         ssl: true,
     }
 })
